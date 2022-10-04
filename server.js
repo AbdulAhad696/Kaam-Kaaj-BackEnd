6dfb21c3730622a8828de6be005381e1818d042d
Huzaifa-crypton <huzaifa18072002@gmail.com>
2022-10-04 21:50:23 +0500

    Backend initial configuration along with database connection

---
+++ server.js
@@ -0,0 +1,28 @@
+// Imports
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import routes from "./routes/route.js"
+//App Config
+const app = express();
+const port = process.env.PORT || 8001;
+const connection_url = "mongodb+srv://kaamkaaj:kaamkaaj123@semesterproject.g48g12p.mongodb.net/?retryWrites=true&w=majority";
+
+//MiddleWares
+app.use(express.json());
+app.use(cors());
+//DB CONFIG
+mongoose.connect(connection_url)
+
+//Routes
+app.use("/" , routes)
+
+//Testing Endpoint
+app.get("/" , (req , res)=>{
+    res.status(200).send("HELLO KAAM KAAJ!!!");
+})
+// Listener
+app.listen(port , ()=>console.log(`LISTENING TO PORT ${port}`))
+
+
+
