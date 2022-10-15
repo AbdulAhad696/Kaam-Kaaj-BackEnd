96cce861e729de2e1f787534c461b3ad9cf4d256
KashirSaeed <92178175+KashirSaeed@users.noreply.github.com>
2022-10-15 12:23:23 -0700

    done with backend of services

---
+++ modules/Services.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const serviceSchema = new mongoose.Schema({
+    tittle: String,
+    image: String,
+    rating: Number,
+});
+
+export default mongoose.model("services",serviceSchema)
