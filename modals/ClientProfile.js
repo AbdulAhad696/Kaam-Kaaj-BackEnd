60041dea50b18e824fcad9d7b92e6fa65555f1f7
Umair Ahmed <umairahmedpaki7@gmail.com>
2022-11-28 20:48:02 +0500

    Creating Client Profile

--- modals/ClientProfile.js
+++ modals/ClientProfile.js
@@ -3,5 +3,9 @@ import mongoose from "mongoose";
 const clientProfileSchema = new mongoose.Schema({
     totalSpending:Number,
     jobs:[{type:mongoose.Schema.Types.ObjectId , ref :"Jobs"}],
+    rating:Number,
+    client:{type:mongoose.Schema.Types.ObjectId , ref:"User"},
+    profileImage:String
+    
 })
-export default mongoose.model("clientProfile",clientProfileSchema);
+export default mongoose.model("clientProfiles",clientProfileSchema);
